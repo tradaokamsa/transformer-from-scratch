176db8233b2b5928827e619f30be07e846fe3dd1
tradaokamsa <hainamdao05@gmail.com>
2024-12-12 18:20:32 +0700

    initial commit

---
+++ model.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbedding(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size,d_model)
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionEmcoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+
+        # create a matrix of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        
+        # apply sin to even position
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # apply cos to odd position
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)    #(1, seq_len, d_model)
+
+        self.register_buffer('pe', pe)  # register the buffer to save the tensor in the model
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) #multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) #added
+    
+    def forward(self, x):
+        
